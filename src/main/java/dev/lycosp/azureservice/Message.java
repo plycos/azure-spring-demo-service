c3d9d43c080354927375f3d01b1f117298ae44c5
Peter Lycos <plycos@gmail.com>
2025-07-16 22:52:47 -0400

    update api

--- src/main/java/dev/lycosp/azureservice/Message.java
+++ src/main/java/dev/lycosp/azureservice/Message.java
@@ -1,10 +1,16 @@
 package dev.lycosp.azureservice;
 
 public class Message {
+    private int rand;
     private String message;
 
-    public Message(String message) {
+    public Message(int rand, String message) {
+        this.rand = rand;
         this.message = message;
+    }
+
+    public int getRand() {
+        return rand;
     }
 
     public String getMessage() {
--- src/main/java/dev/lycosp/azureservice/MessageController.java
+++ src/main/java/dev/lycosp/azureservice/MessageController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/message")
 public class MessageController {
     @GetMapping
     public Message message() {
-        return new Message("Hello World!!!");
+        Random r = new Random();
+        int num = r.nextInt(10000);
+        return new Message(num, "Hello World!!!");
     }
 }
 
